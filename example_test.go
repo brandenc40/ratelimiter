4d9cdb11f351bd9c68d5eea41cca9db3420204bd
Branden Colen <brandencolen@gmail.com>
2022-01-15 13:30:08 -0600

    added example

--- example_test.go
+++ example_test.go
@@ -1,6 +1,7 @@
 package ratelimiter_test
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/brandenc40/ratelimiter"
@@ -12,10 +13,28 @@ func Example() {
 		ratelimiter.WithMaxQueueSize(100), // max of 100 requests queued up before failure
 	)
 
+	var (
+		start    = time.Now()
+		nSuccess = 0
+		nError   = 0
+	)
 	for i := 0; i < 100; i++ {
 		if err := rl.Wait(); err != nil {
-			// handle err
+			nError++
+		} else {
+			nSuccess++
 		}
-		// do some rate limited functionality
 	}
+
+	elapsed := time.Since(start)
+
+	// 10ms each for 100 requrests == 990ms total (first request is 0ms)
+	fmt.Printf("(timeElapsed >= 990ms) == %v\n", elapsed.Milliseconds() >= 950)
+	fmt.Println("nSuccess:", nSuccess)
+	fmt.Println("nError:", nError)
+
+	// Output:
+	// (timeElapsed >= 990ms) == true
+	// nSuccess: 100
+	// nError: 0
 }
